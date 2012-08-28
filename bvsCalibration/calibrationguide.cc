6c726585dd2c11cea3ba4a43be732ae74fea36cb
nilsonholger <nilsonholger@hyve.org>
2012-08-29 00:08:21 +0200

    guide: add comments and fix reorder functionality

--- bvsCalibration/calibrationguide.cc
+++ bvsCalibration/calibrationguide.cc
@@ -1,30 +1,35 @@
 #include "calibrationguide.h"
-
 #include<iostream>
 
 
-CalibrationGuide::CalibrationGuide(int& numImages, int& numDetections, float scale, int centerDetections, int sectorDetections)
-	: numImages(numImages)
-	, numDetections(numDetections)
-	, scale(scale)
-	, centerDetections(centerDetections)
-	, sectorDetections(sectorDetections)
-	, state(0)
-	, imgDiag(0)
-	, size{0, 0}
-	, center{0, 0}
-	, p1{0, 0}
-	, p2{0, 0}
-	, detCenter{0, 0}
-	, color{0, 0, 255}
-{
-	numImages = centerDetections + 8 * sectorDetections;
-}
+
+CalibrationGuide::CalibrationGuide(const int& numImages, const int& numDetections, float scale, int centerDetections, int sectorDetections)
+	: numImages(numImages),
+	numDetections(numDetections),
+	scale(scale),
+	centerDetections(centerDetections),
+	sectorDetections(sectorDetections),
+	state(0),
+	imgDiag(0),
+	size{0, 0},
+	center{0, 0},
+	p1{0, 0},
+	p2{0, 0},
+	detCenter{0, 0},
+	color{0, 0, 255}
+{ }
 
 
 
 void CalibrationGuide::addTargetOverlay(cv::Mat& img)
 {
+	if (numImages!=centerDetections+8*sectorDetections)
+	{
+		cv::putText(img, "WARNING: CHECK numDetections == centerDetections + 8 * sectorDetections!",
+				cv::Point(10, img.rows/2), CV_FONT_HERSHEY_SIMPLEX, 1.0f, cv::Scalar(0, 0, 255));
+		return;
+	}
+
 	float border;
 	if (img.cols != size.x || img.rows != size.y) updateInputSize(img);
 
@@ -53,22 +58,37 @@ void CalibrationGuide::addTargetOverlay(cv::Mat& img)
 
 bool CalibrationGuide::checkDetectionQuality(cv::Mat& img, std::vector<cv::Point2f>& points)
 {
-	int diag = std::sqrt(std::pow(points.back().x-points.front().x, 2) + std::pow(points.back().y-points.front().y, 2));
+	int diag = std::sqrt(std::pow(points.back().x-points.front().x, 2)
+			+ std::pow(points.back().y-points.front().y, 2));
 	detCenter.x = (points.front().x+points.back().x)/2;
 	detCenter.y = (points.front().y+points.back().y)/2;
 	cv::line(img, cv::Point(center.x, center.y), cv::Point(detCenter.x, detCenter.y), color, 2);
 	color = {0, 0, 255};
 
+	bool returnValue = true;
 	switch (state)
 	{
 		case 0:
-			if (diag < imgDiag*scale) return false;
+			if (diag < imgDiag*scale) returnValue = false;
 			if (numDetections >= centerDetections) state++;
+			cv::putText(img, "GREEN: detected pattern size ok.",
+					cv::Point(10, img.rows-40), CV_FONT_HERSHEY_SIMPLEX,
+					1.0f, cv::Scalar(0, 255, 0));
+			cv::putText(img, "RED: detected pattern size to small, move closer.",
+					cv::Point(10, img.rows-10), CV_FONT_HERSHEY_SIMPLEX,
+					1.0f, cv::Scalar(0, 0, 255));
 			break;
 		case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8:
 			if (numDetections < centerDetections+sectorDetections*(state-1)) state--;
-			if (diag > imgDiag/2) return false;
-			if (std::abs(detCenter.x-center.x)>size.x/6 || std::abs(detCenter.y-center.y)>size.y/6) return false;
+			if (diag > imgDiag/2) returnValue = false;
+			if (std::abs(detCenter.x-center.x)>size.x/6 || std::abs(detCenter.y-center.y)>size.y/6)
+				returnValue = false;
+			cv::putText(img, "GREEN: pattern center position ok.",
+					cv::Point(10, img.rows-40), CV_FONT_HERSHEY_SIMPLEX,
+					1.0f, cv::Scalar(0, 255, 0));
+			cv::putText(img, "RED: pattern center outside of target.",
+					cv::Point(10, img.rows-10), CV_FONT_HERSHEY_SIMPLEX,
+					1.0f, cv::Scalar(0, 0, 255));
 			if (numDetections >= centerDetections+sectorDetections*state) state++;
 			break;
 		default:
@@ -76,28 +96,26 @@ bool CalibrationGuide::checkDetectionQuality(cv::Mat& img, std::vector<cv::Point
 	}
 
 	color = {0, 255, 0};
-	return true;
+
+	return returnValue;
 }
 
 
 
 void CalibrationGuide::reorderDetections(std::vector<std::vector<cv::Point2f>>& points)
 {
-	//leave #centerDetections alone
-	//start at #centerDetections
 	std::vector<std::vector<cv::Point2f>> tmp;
 
 	for (int i=0; i<sectorDetections; i++)
 	{
-		// offset -1 cause vector index starts at 0
-		tmp.push_back(points.at(centerDetections));
-		tmp.push_back(points.at(centerDetections+4*i));
-		tmp.push_back(points.at(centerDetections+6*i));
-		tmp.push_back(points.at(centerDetections+2*i));
-		tmp.push_back(points.at(centerDetections+7*i));
-		tmp.push_back(points.at(centerDetections+3*i));
-		tmp.push_back(points.at(centerDetections+1*i));
-		tmp.push_back(points.at(centerDetections+5*i));
+		tmp.push_back(points.at(centerDetections+i));
+		tmp.push_back(points.at(centerDetections+i+3*sectorDetections));
+		tmp.push_back(points.at(centerDetections+i+6*sectorDetections));
+		tmp.push_back(points.at(centerDetections+i+1*sectorDetections));
+		tmp.push_back(points.at(centerDetections+i+4*sectorDetections));
+		tmp.push_back(points.at(centerDetections+i+7*sectorDetections));
+		tmp.push_back(points.at(centerDetections+i+2*sectorDetections));
+		tmp.push_back(points.at(centerDetections+i+5*sectorDetections));
 	}
 
 	points.erase(points.begin()+centerDetections, points.end());
@@ -121,3 +139,4 @@ void CalibrationGuide::updateTarget(int sectorX, int sectorY)
 	p2 = {(sectorX+1)*size.x/3, (sectorY+1)*size.y/3};
 	center = {(p2.x+p1.x)/2, (p2.y+p1.y)/2};
 }
+
--- bvsCalibration/calibrationguide.h
+++ bvsCalibration/calibrationguide.h
@@ -2,72 +2,139 @@
 #define CALIBRATION_GUIDE_H
 
 #include<vector>
-
 #include "opencv2/opencv.hpp"
 
 
-/**
- * TODO show usage information on startup
- *
- * sector/state relationships:
- * @code
- *   00  |  10  |  20
- *    1  |   2  |   3
- * --------------------
- *   01  |  11  |  21
- *    8  |   0  |   4
- * --------------------
- *   02  |  12  |  22
- *    7  |   6  |   5
- * @endcode
- *
+
+/** Calibration Guidance System.
+ * This is a guidance system to improve the quality of calibration images, or
+ * to be more precise, the position and size of the detected patterns. The
+ * purpose of this is to increase absolute pixel coverage by the detected
+ * patterns.
+ * The system separates detections into large center detections and smaller
+ * per sector detections (of which there are 8). The calibration guide will
+ * first collect center detections and then per sector detections.
+ * If desired, it will reorder the sector detections (by using a star
+ * pattern) to further improve the calibration quality.
  */
 class CalibrationGuide
 {
 	public:
-		CalibrationGuide(int& numImages, int& numDetections, float scale, int centerDetections, int sectorDetections);
+		/** CalibrationGuide constructor.
+		 * @param[in] numImages The overall number of images.
+		 * This should be centerDetections + 8 * sectorDetections.
+		 * @param[in] numDetections The number of pattern detections so far.
+		 * @param[in] scale The scale of center detections in percent of the image diagonal.
+		 * @param[in] centerDetections Number of center detections.
+		 * @param[in] sectorDetections Number of per sector detections.
+		 */
+		CalibrationGuide(const int& numImages, const int& numDetections, float scale, int centerDetections, int sectorDetections);
 
+		/** Draws target information.
+		 * This method draws target information on an image. These consists of a
+		 * rectangle of the desired size and position.
+		 * @param[in] img The image to draw on.
+		 */
 		void addTargetOverlay(cv::Mat& img);
+
+		/** Checks detected pattern quality.
+		 * Given an image and the detected points, this method checks the quality
+		 * of the detection. It does so by calculating the patterns diagonal and
+		 * center position and comparing them with the requirements.
+		 * It also draws a line onto the image showing the distance between the
+		 * center of the detected pattern and the desired position.
+		 * @param[in] img The image to draw on.
+		 * @param[in] points Vector of detected pattern points.
+		 * @return True if the detection satisfies all conditions, false otherwise.
+		 */
 		bool checkDetectionQuality(cv::Mat& img, std::vector<cv::Point2f>& points);
+
+		/** Reorders the detections.
+		 * IMPORTANT: This method has to be called for every vector of detected
+		 * points (for stereo or multi camera scenarios, every vector will be
+		 * reorderer the same way, otherwise stereo information would be
+		 * destroyed).
+		 *
+		 * This method can reorder the pattern detections used for the calibration
+		 * to further improve the calibration quality (by increasing diversity
+		 * between consecutive detections).
+		 *
+		 * The reorder pattern resembles a star, thus destroying the linear order
+		 * of sector detections and decreasing calibration distortion
+		 *
+		 * sector->order:
+		 *
+		 * @code
+		 *  1->1 | 2->4 | 3->7
+		 * --------------------
+		 *  8->6 |      | 4->2
+		 * --------------------
+		 *  7->3 | 6->8 | 5->5
+		 * @endcode
+		 *
+		 * @params[in] points Vector of points to reorder.
+		 */
 		void reorderDetections(std::vector<std::vector<cv::Point2f>>& points);
 
 	private:
+		/** Update input size.
+		 * @param[in] img Image to retrieve size from.
+		 */
 		void updateInputSize(cv::Mat& img);
+
+		/** Modifies the target position.
+		 *
+		 * @code
+		 * sector: XY
+		 * state: n
+		 *
+		 *   00  |  10  |  20
+		 *    1  |   2  |   3
+		 * --------------------
+		 *   01  |  11  |  21
+		 *    8  |   0  |   4
+		 * --------------------
+		 *   02  |  12  |  22
+		 *    7  |   6  |   5
+		 * @endcode
+		 *
+		 * @param[in] sectorX X coordinate of sector.
+		 * @param[in] sectorY Y coordinate of sector.
+		 */
 		void updateTarget(int sectorX, int sectorY);
 
-		int& numImages;
-		int& numDetections;
-		float scale;
-		int centerDetections;
-		int sectorDetections;
-		int state;
-		int imgDiag;
+		const int& numImages; /**< Overall number of images used for calibration. */
+		const int& numDetections; /**< Number of actual detections. */
+		float scale; /**< Center detection scale in percent. */
+		int centerDetections; /**< Number of center detections. */
+		int sectorDetections; /**< Number of per sector detections. */
 
+		/** Detection state.
+		 * @li \c 0: large center detections until numDetections = centerDetections,
+		 * check if size of detection > targetSize
+		 * @li \c 1-8: sector detections until numDetections = centerDetections and
+		 * state * sectorDetections, check if size of detection > (targetSize/3) and
+		 * if center close to sector center
+		 */
+		int state;
+		int imgDiag; /**< Lenght of image diagonal. */
+
+		/** Point structure. */
 		struct point
 		{
-			float x;
-			float y;
+			float x; /**< X position. */
+			float y; /**< Y position. */
 		};
 
-		point size;
-		point center;
-		point p1;
-		point p2;
-		point detCenter;
-		cv::Scalar color;
-		//TODO
-		// check in module or set: numImages = centerDetections + sectorDetections*8
-		// option: reorder detections -> center -> alternate between sectors in each detection
-		// functions need to act depending on state:
-		// state 0: large center until numDetections = centerDetections, state++;
-		//		check: size of detection > targetSize
-		// state 1-8: sector until numDetections = centerDetections + state * sectorDetections, state++
-		//		check: size of detection > (targetSize/3) and center close to sector center
-		// state 9: signal done
-		//
-		// add function to reorder detections to improve calibration distribution
+		point size; /**< Size of image. */
+		point center; /**< Target center. */
+		point p1; /**< Upper left target point. */
+		point p2; /**< Lower right target point. */
+		point detCenter; /**< Detection center. */
+		cv::Scalar color; /**< Target rectangle color. */
 };
 
 
 
 #endif // CALIBRATION_GUIDE_H
+
