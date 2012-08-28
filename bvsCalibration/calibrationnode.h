434fc44c95cd980687fdb2d0768faac35c15e6fa
nilsonholger <nilsonholger@hyve.org>
2012-08-29 00:06:59 +0200

    node: add comments and some additional metadata

--- bvsCalibration/calibrationnode.h
+++ bvsCalibration/calibrationnode.h
@@ -2,13 +2,12 @@
 #define CALIBRATION_NODE_H
 
 #include<vector>
-
 #include "bvs/connector.h"
 #include "opencv2/opencv.hpp"
 
 
 
-// forward declaration
+// Forward declaration.
 struct CalibrationNode;
 
 
@@ -21,22 +20,29 @@ typedef std::vector<CalibrationNode*> CalNodeVec;
 /** Calibration node meta data. */
 struct CalibrationNode
 {
-	CalibrationNode( int id,
-	BVS::Connector<cv::Mat> input, /**< Input connector. */
-	BVS::Connector<cv::Mat> output, /**< Output connector. */
-	cv::Mat frame, /**< Input frame. */
-	cv::Mat scaledFrame, /**< Scaled input frame. */
-	std::vector<cv::Point2f> framePoints, /**< Current frame points. */
-	cv::Mat sample, /**< Full size sample. */
-	std::vector<cv::Point2f> points, /**< Detected points. */
-	std::vector<std::vector<cv::Point2f>> pointStore, /**< Store of detected points. */
-	cv::Mat cameraMatrix, /**< Internal camera matrix. */
-	cv::Mat distCoeffs, /**< Distortion coefficients. */
-	cv::Mat rectificationMatrix, /**< Rectification matrix. */
-	cv::Mat projectionMatrix /**< Projection matrix. */
-	)
-		:
-		id(id),
+	/** CalibrationNode Constructor.
+	 * @param[in] id Identifier.
+	 * @param[in] input Input connector.
+	 * @param[in] output Output connector.
+	 * @param[in] frame Input frame.
+	 * @param[in] scaledFrame Scaled version of input frame.
+	 * @param[in] framePoints Detected pattern points in scaled frame.
+	 * @param[in] sample Full size sample copy for detection.
+	 * @param[in] points Detected pattern points in sample.
+	 * @param[in] pointStore Vector of detected points.
+	 * @param[in] cameraMatrix Camera intrinsics.
+	 * @param[in] distCoeffs Distortion coefficients.
+	 * @param[in] rectificationMatrix Rectification matrix.
+	 * @param[in] projectionMatrix Projection matrix.
+	 * @param[in] homographyMatrix Homography matrix.
+	 * @param[in] validRegionOfInterest Calculated valid region of interest.
+	 */
+	CalibrationNode(int id, BVS::Connector<cv::Mat> input, BVS::Connector<cv::Mat> output,
+			cv::Mat frame, cv::Mat scaledFrame, std::vector<cv::Point2f> framePoints, cv::Mat sample,
+			std::vector<cv::Point2f> points, std::vector<std::vector<cv::Point2f>> pointStore,
+			cv::Mat cameraMatrix, cv::Mat distCoeffs, cv::Mat rectificationMatrix, cv::Mat projectionMatrix,
+			cv::Mat homographyMatrix, cv::Rect validRegionOfInterest)
+		: id(id),
 		input(input),
 		output(output),
 		frame(frame),
@@ -48,8 +54,10 @@ struct CalibrationNode
 		cameraMatrix(cameraMatrix),
 		distCoeffs(distCoeffs),
 		rectificationMatrix(rectificationMatrix),
-		projectionMatrix(projectionMatrix)
-		{}
+		projectionMatrix(projectionMatrix),
+		homographyMatrix(homographyMatrix),
+		validRegionOfInterest(validRegionOfInterest)
+	{ }
 
 	int id; /**< Identifier. */
 	BVS::Connector<cv::Mat> input; /**< Input connector. */
@@ -64,6 +72,8 @@ struct CalibrationNode
 	cv::Mat distCoeffs; /**< Distortion coefficients. */
 	cv::Mat rectificationMatrix; /**< Rectification matrix. */
 	cv::Mat projectionMatrix; /**< Projection matrix. */
+	cv::Mat homographyMatrix; /**< Homography matrix. */
+	cv::Rect validRegionOfInterest; /**< Calculated valid region of interest. */
 };
 
 
